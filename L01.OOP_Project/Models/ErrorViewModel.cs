b007ceb29fd8f99b8b4cf053fbf7cd5e60bba9c4
Zehra Seren <fatmazehraseren@gmail.com>
2024-10-23 18:17:56 +0300

    OOP_Project is created.

---
+++ L01.OOP_Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01.OOP_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
