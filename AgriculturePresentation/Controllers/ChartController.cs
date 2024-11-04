cc91906c3397cf4d339a31f66d6aa4ea5fefa5ef
Zehra Seren <fatmazehraseren@gmail.com>
2024-11-04 20:34:32 +0300

    ChartController is done.

---
+++ AgriculturePresentation/Controllers/ChartController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Agriculture.Presentation.Models;
+
+namespace Agriculture.Presentation.Controllers
+{
+    public class ChartController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProductChart()
+        {
+            List<ProductClass> products = new List<ProductClass>();
+            products.Add(new ProductClass
+            {
+                productname = "Buğday",
+                productvalue = 850
+            });
+
+            products.Add(new ProductClass
+            {
+                productname = "Mercimek",
+                productvalue = 480
+            });
+
+            products.Add(new ProductClass
+            {
+                productname = "Arpa",
+                productvalue = 250
+            });
+
+            products.Add(new ProductClass
+            {
+                productname = "Pirinç",
+                productvalue = 120
+            });
+
+            products.Add(new ProductClass
+            {
+                productname = "Domates",
+                productvalue = 960
+            });
+
+            return Json(new { jsonList = products });
+        }
+    }
+}
---
+++ AgriculturePresentation/Models/ProductClass.cs
@@ -0,0 +1,8 @@
+﻿namespace Agriculture.Presentation.Models
+{
+    public class ProductClass
+    {
+        public string productname { get; set; }
+        public int productvalue { get; set; }
+    }
+}
